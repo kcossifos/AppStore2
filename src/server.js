895f90f79e48970ca92fd413f84bf935dfb29d3b
Kelsey Cossifos <kcossifos@gmail.com>
2016-08-04 12:21:41 -0400

    Updated Server File

--- src/server.js
+++ src/server.js
@@ -1,15 +1,15 @@
-var express = require('express');
-var body_parser = require('body-parser');
-var app = express();
+const express = require('express');
+const body_parser = require('body-parser');
+const app = express();
 
 //port used for the app
-var port = 3000;
+const port = 3000;
 
-//App is required to use /api
+//App is required to use /routes/index
 require('./routes/index')(app);
 
 //App listens to which port the server is on
-var server = app.listen(port, () => {
+const server = app.listen(port, () => {
 	console.log('Server on', port);
 });
 
