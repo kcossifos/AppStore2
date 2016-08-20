0a29e0ff3096aa26ffa89e4ea30ac091357bdda5
Kelsey Cossifos <kcossifos@gmail.com>
2016-08-19 21:32:14 -0400

    update json

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,16 @@
 module.exports = (express) => {
   const router = express.Router();
 
-// // checks status
-//   router.get('/status', (req, res) => {
-//     res.json({
-//       healthy: true,
-//     });
-//   });
-//
-//   router.get('/', (req, res) => {
-//     res.json('Check out this dynamic api');
-//   });
+// checks status
+  router.get('/status', (req, res) => {
+    res.json({
+      healthy: true,
+    });
+  });
+
+  router.get('/', (req, res) => {
+    res.json('Check out this dynamic api');
+  });
 
 // creates route
   router.use('/api/', require('./api/v1/users')(express));
