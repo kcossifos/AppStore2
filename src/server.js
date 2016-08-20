df838a55f962e987776a1916255cd863035f839a
Kelsey Cossifos <kcossifos@gmail.com>
2016-08-20 15:18:05 -0400

    update json

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const util = require('kelTool2');
 const app = express();
 
 // port used for the app
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
